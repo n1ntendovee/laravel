4fa79585d89674b1df002ce533e9be30e9194edf
n1ntendovee <nazar.kozak.3010@gmail.com>
2019-10-27 13:29:00 +0200

    addons

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', ['name' => 'n1n']);
+});
+
+Route::get('/contacts', function(){
+    return view('contacts');
 });
